442fe34fd6e109713c681c04a094649a413e7db3
EloáVM22 <127448572+EloaVasconcelos@users.noreply.github.com>
2023-06-20 10:07:07 -0300

    Atualizar o MainActivity2quarta.java
    novidades

--- app/src/main/java/com/example/sustentavelaap/MainActivity2quarta.java
+++ app/src/main/java/com/example/sustentavelaap/MainActivity2quarta.java
@@ -10,5 +10,21 @@ public class MainActivity2quarta extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activity2quarta);
+
+
+
+
+
+findViewById(R.id.bntvoltar1); 
+  
+
+btnsus.setOnClickListener(new View.OnClickListener() { 
+             public void onClick(View vo) { 
+                 Intent voltar1 = new Intent(MainActivity2quarta.this TerceiraMainActivity2.class); 
+                 startActivity(voltar1); 
+  
+ 
+                 
+
     }
 }
