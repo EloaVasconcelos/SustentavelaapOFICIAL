714e194a9eaa54dc8f0fc1be63f54b5ae39a75a3
EloáVM22 <127448572+EloaVasconcelos@users.noreply.github.com>
2023-06-20 10:09:28 -0300

    Atualizar o MainActivity2quinta.java
    voltando a tela3

--- app/src/main/java/com/example/sustentavelaap/MainActivity2quinta.java
+++ app/src/main/java/com/example/sustentavelaap/MainActivity2quinta.java
@@ -9,6 +9,15 @@ public class MainActivity2quinta extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_activity2quinta);
+        setContentView(R.layout.activity_main_activity2quinta)
+
+
+
+findViewById(R.id.bntvoltar2);  
+  
+ btnsus.setOnClickListener(new View.OnClickListener() {  
+              public void onClick(View vol) {  
+                  Intent voltar2 = new Intent(MainActivity2quarta.this TerceiraMainActivity2.class);  
+                  startActivity(voltar2); 
     }
 }
