68f0ae91720c1729968fff5e55bb973f2d1278f9
EloáVM22 <127448572+EloaVasconcelos@users.noreply.github.com>
2023-06-20 10:13:26 -0300

    Atualizar o MainActivity2sexta.java
    volta para a tela3

--- app/src/main/java/com/example/sustentavelaap/MainActivity2sexta.java
+++ app/src/main/java/com/example/sustentavelaap/MainActivity2sexta.java
@@ -10,5 +10,16 @@ public class MainActivity2sexta extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activity2sexta);
-    }
+
+
+
+findViewById(R.id.bntvoltar3);  
+  
+  
+ btnsus.setOnClickListener(new View.OnClickListener() {  
+              public void onClick(View volta) {  
+                  Intent voltar3 = new Intent(MainActivity2quarta.this TerceiraMainActivity2.class);  
+                  startActivity(voltar3);  
+
+   }
 }
