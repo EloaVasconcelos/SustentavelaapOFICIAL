6b111add8bdbf0ba827ff8f930d7b2d769bba9df
EloáVM22 <127448572+EloaVasconcelos@users.noreply.github.com>
2023-06-20 09:41:40 -0300

    Atualizar o TerceiraMainActivity2.java
    atualização nova

--- app/src/main/java/com/example/sustentavelaap/TerceiraMainActivity2.java
+++ app/src/main/java/com/example/sustentavelaap/TerceiraMainActivity2.java
@@ -10,5 +10,31 @@ public class TerceiraMainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_terceira_main3);
+
+
+findViewById(R.id.btnsus);
+
+findViewById(R.id.btnreci);
+
+findViewById(R.id.btnpontos);
+
+        btnsus.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View s) {
+                Intent susten = new Intent(terceiraMainActivity2.this MainterceiraActivity.class);
+                startActivity(susten);
+
+
+        btnreci.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View n) {
+                Intent recicla = new Intent(terceiraMainActivity2.this MainterceiraActivity.class);
+                startActivity(recicla);
+
+
+        btnpontos.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View c) {
+                Intent pontos  = new Intent(terceiraMainActivity2.this MainterceiraActivity.class);
+                startActivity(pontos);
+
+
     }
 }
